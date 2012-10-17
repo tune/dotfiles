e65809d16885ee5d4a9a599a7186190532770e95
tune <Tsunematsu_Yuichi@cebm.canon.com.hk>
2012-10-17 09:11:36 +0800

    全リポジトリで共通の.gitignoreをgithubのものから作る形に変更

--- setup.sh
+++ setup.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
 
-DOT_FILES=( .gitconfig .gitignore .irbrc .screenrc .vimrc .zshrc )
+# Create symbolic link
+DOT_FILES=(
+	.gitconfig
+	.irbrc
+	.screenrc
+	.vimrc
+	.zshrc
+)
 
 for file in ${DOT_FILES[@]}
 do
     ln -s $HOME/dotfiles/$file $HOME/$file
+done
+
+
+# Create .global_ignore for git
+IGNORE_FILES=(
+	Autotools.gitignore
+	C++.gitignore
+	C.gitignore
+	Ruby.gitignore
+	Global/Linux.gitignore
+	Global/SVN.gitignore
+	Global/VisualStudio.gitignore
+	Global/Windows.gitignore
+	Global/vim.gitignore
+)
+
+for file in ${IGNORE_FILES[@]}
+do
+    echo "###########################################################" >> .global_ignore
+    echo "# From $file" >> .global_ignore
+    cat "git/gitignore/$file" >> .global_ignore
+    echo $'\n' >> .global_ignore
 done
